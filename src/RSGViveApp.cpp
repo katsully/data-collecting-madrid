35f4230383958b43f209f9ba4eaf415ec1d2bc73
Kat Sullivan <kmsullivan012@gmail.com>
2018-07-17 17:38:08 -0400

    added header files and heading the first method declarations

--- src/RSGViveApp.cpp
+++ src/RSGViveApp.cpp
@@ -2,9 +2,36 @@
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
 
+// from hellovr_opengl_main.cpp
+//#include <SDL.h>
+#include <openvr.h>
+
 using namespace ci;
 using namespace ci::app;
 using namespace std;
+
+// From hellovr_opengl_maincpp
+// vr namespace comes from openvr.h
+class CGLRenderModel {
+	CGLRenderModel(const std::string & srenderModelName);
+	~CGLRenderModel();
+
+	bool bInit(const vr::RenderModel_t & vrModel, const vr::RenderModel_TextureMap_t & vrDiffuseTexture);
+	void Cleanup();
+	void Draw();
+	const std::string & getName() const { return m_sModelName; }
+
+private:
+	GLuint m_glVertBuffer;
+	GLuint m_glIndexBuffer;
+	GLuint m_glVertArray;
+	GLuint m_glTexture;
+	GLsizei m_unVectexCount;
+	std::string m_sModelName;
+};
+
+static bool g_bPrintf = true;
+
 
 class RSGViveApp : public App {
   public:
@@ -12,7 +39,21 @@ class RSGViveApp : public App {
 	void mouseDown( MouseEvent event ) override;
 	void update() override;
 	void draw() override;
+
+	// from hellovr_opengl_main.cpp
+	RSGViveApp(int argc, char *argv[]);
+	virtual ~RSGViveApp();
+
+	bool bInit();
+	bool bInitGL();
+	bool bInitCompositor();
+	
+	void shutdown();
+
+	void runMainLoop();
+
 };
+
 
 void RSGViveApp::setup()
 {
